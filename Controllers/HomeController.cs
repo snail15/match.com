11e75ce6d8288aa51a2f3c6ab2d767ed2b81fce9
Brooks Von Doom <jeffery.brooks@loop.colum.edu>
2017-09-25 16:41:08 -0500

    message route setup

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -3,12 +3,22 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Http;
+using match.Models;
+using System.Linq;
 
 namespace match.Controllers
 {
     public class HomeController : Controller
     {
-        // GET: /Home/
+        private MatchContext context;
+
+        public HomeController(MatchContext _context)
+        {
+            context = _context;
+        }
+// =============================================================
+// =============================================================
+
         [HttpGet]
         [Route("")]
         public IActionResult Index()
@@ -42,6 +52,39 @@ namespace match.Controllers
         [Route("/bodytype")]
         public IActionResult BodyType() {
             return View();
+        }
+// =============================================================
+// =============================================================
+        [HttpPost]
+        [Route("/sendMessage")]
+        public IActionResult sendMessage(MessageViewModel model) 
+        {
+             int? userId = HttpContext.Session.GetInt32("currentUserId");
+            
+            User currentUser = context.Users.SingleOrDefault(user => user.Id == userId);
+
+            if(ModelState.IsValid)
+            {
+                Message newMessage = new Message 
+                {
+                    messageContent = model.messageContent,
+                    senderId = currentUser.Id,
+                    receiverId = ,
+                    created_at = DateTime.Now,
+                    updated_at = DateTime.Now
+                };
+                context.Add(newMessage);
+                context.SaveChanges();
+                return RedirectToAction("dashBoard");
+            } 
+            else
+            {
+                ViewBag.User = currentUser;
+                
+                return View(model);
+            }
+
+            return View(model);
         }
 // =============================================================
 // =============================================================
