b2c48d2980ef85564793d8aca8decfab0640169a
Brooks Von Doom <jeffery.brooks@loop.colum.edu>
2017-09-28 10:41:17 -0500

    messaging almost done

--- Models/Message.cs
+++ Models/Message.cs
@@ -6,13 +6,17 @@ namespace match.Models
     public class Message : BaseEntity {
 
         public int Id {get; set;}
+
         public int senderId {get; set;}
+        public Conversing Sender {get; set;}
+
         public int receiverId {get; set;}
+        public Conversing Receiver {get; set;}
+        
         public string messageContent {get; set;}
         public DateTime created_at {get; set;}
         public DateTime updated_at {get; set;}
-        public User Sender { get; set; }
-        public User Receiver { get; set; }
+        
     }
     
 }
